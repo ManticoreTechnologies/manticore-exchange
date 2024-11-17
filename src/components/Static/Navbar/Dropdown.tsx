1b97467286b3d089d579bfdd55bcd693365180b9
Manticore <development@manticore.technology>
2024-11-16 19:14:55 -0500

    Navbar styles: hide links based on page width

--- src/components/Static/Navbar/Dropdown.tsx
+++ src/components/Static/Navbar/Dropdown.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { FaBars, FaBlog, FaRoad, FaDatabase, FaChartBar } from 'react-icons/fa';
+import { FaBars, FaBlog, FaRoad, FaDatabase, FaChartBar, FaUser, FaTint, FaExchangeAlt, FaSearch } from 'react-icons/fa';
 import './Dropdown.css';
 
 interface DropdownProps {
   isOpen: boolean;
   toggleDropdown: () => void;
+  showSearch: boolean;
+  showTrade: boolean;
+  showFaucet: boolean;
+  showProfile: boolean;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ isOpen, toggleDropdown }) => {
+const Dropdown: React.FC<DropdownProps> = ({ isOpen, toggleDropdown, showSearch, showTrade, showFaucet, showProfile }) => {
   return (
     <div className="dropdown" aria-expanded={isOpen}>
       <button 
@@ -25,6 +29,19 @@ const Dropdown: React.FC<DropdownProps> = ({ isOpen, toggleDropdown }) => {
           <a href="/roadmap"><FaRoad /> Roadmap</a>
           <a href="/ipfs"><FaDatabase /> IPFS</a>
           <a href="/chart"><FaChartBar /> Chart</a>
+          {!showProfile && (
+            <a href="/profile"><FaUser /> Profile</a>
+          )}
+          {!showFaucet && (
+            <a href="/faucet"><FaTint /> Faucet</a>
+          )}
+          {!showTrade && (
+            <a href="/trade"><FaExchangeAlt /> Trade</a>
+          )}
+          {!showSearch && (
+            <a href="/search"><FaSearch /> Search</a>
+          )}
+          
         </div>
       )}
     </div>
--- src/components/Static/Navbar/Navbar.tsx
+++ src/components/Static/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import logo from '../../../images/enhanced_logo_old.png';
@@ -12,6 +12,8 @@ const ThemeToggleButton: React.FC = () => {
   const [theme, setTheme] = useState(
     document.body.getAttribute("data-theme") || "dark"
   );
+
+
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
@@ -30,6 +32,32 @@ const ThemeToggleButton: React.FC = () => {
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [showSearch, setShowSearch] = useState(true);  
+  const [showTrade, setShowTrade] = useState(true);
+  const [showFaucet, setShowFaucet] = useState(true);
+  const [showProfile, setShowProfile] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+      setIsMobile(window.innerWidth < 768);
+      setShowSearch(window.innerWidth >= 700);
+      setShowTrade(window.innerWidth >= 600);
+      setShowFaucet(window.innerWidth >= 500);
+      setShowProfile(window.innerWidth >= 400);
+      console.log(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Call handleResize initially in case the window size is already below 768px
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -59,37 +87,50 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="navbar-links">
-
-          <NavLink to="/search" className="nav-link animated-link" onClick={handleLinkClick}>
-            <div className="nav-link-container">
-              <i className="fas fa-search"></i>
-              <p>Search</p>
-            </div>
-          </NavLink>
-
-          <NavLink to="/trade" className="nav-link animated-link" onClick={handleLinkClick}>
-            <div className="nav-link-container">
-              <i className="fas fa-exchange-alt"></i>
-              <p>Trade</p>
-            </div>
-          </NavLink>
-
-          <NavLink to="/faucet" className="nav-link animated-link" onClick={handleLinkClick}> 
-            <div className="nav-link-container">
-              <i className="fas fa-tint"></i>
-              <p>Faucet</p>
-            </div>
-          </NavLink>
-
+            <span>
+              {showSearch && (
+                <NavLink to="/search" className="nav-link animated-link" onClick={handleLinkClick}>
+                  <div className="nav-link-container">
+                    <i className="fas fa-search"></i>
+                    <p>Search</p>
+                  </div>
+                </NavLink>
+              )}
+              {showTrade && (
+                <NavLink to="/trade" className="nav-link animated-link" onClick={handleLinkClick}>
+                  <div className="nav-link-container">
+                    <i className="fas fa-exchange-alt"></i>
+                    <p>Trade</p>
+                  </div>
+                </NavLink>
+              )}
+              {showFaucet && (
+                <NavLink to="/faucet" className="nav-link animated-link" onClick={handleLinkClick}> 
+                  <div className="nav-link-container">
+                    <i className="fas fa-tint"></i>
+                    <p>Faucet</p>
+                  </div>
+                </NavLink>
+              )}
+            </span>
           
-          <Dropdown isOpen={isMenuOpen} toggleDropdown={handleMenuToggle} />
-          
-        <NavLink to="/profile" className="nav-link animated-link" onClick={handleLinkClick}>
-          <div className="nav-link-container">
-            <i className="fas fa-user"></i>
-            <p>Profile</p>
-          </div>
-        </NavLink>
+          <Dropdown 
+            isOpen={isMenuOpen} 
+            toggleDropdown={handleMenuToggle} 
+            showSearch={showSearch} 
+            showTrade={showTrade} 
+            showFaucet={showFaucet} 
+            showProfile={showProfile} 
+          />
+
+          {showProfile && (
+            <NavLink to="/profile" className="nav-link animated-link" onClick={handleLinkClick}>
+              <div className="nav-link-container">
+                <i className="fas fa-user"></i>
+                <p>Profile</p>
+              </div>
+            </NavLink>
+          )}
         
           </div>
       </div>
